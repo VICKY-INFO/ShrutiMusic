6481a8c93754ec02e0208474db49850179a80192
Vikas N D <theengineerapex@gmail.com>
2025-08-28 14:08:43 +0530

    Create health.py

---
+++ health.py
@@ -0,0 +1,10 @@
+# health.py
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'ShrutiMusic bot is running.'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
